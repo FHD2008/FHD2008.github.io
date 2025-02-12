e2315b1833ec83d329aa1c6d4ca463849110af2c
FHD2008 <fahadshah2008@outlook.com>
2025-02-11 22:38:34 -0600

    added new mountains
    added some items and colors

--- Inteactive Scene Assignment/sketch.js
+++ Inteactive Scene Assignment/sketch.js
@@ -9,13 +9,46 @@ function setup() {
 }
 
 function draw() {
-  background("lightblue");
+  background("skyblue");
+  strokeWeight(5)
   drawGround();
+  drawMount();
+  drawTree(65, 270, 1);
+  drawTree(635, 270, 0.5);
+  fill("red");
+  text(mouseX + ", " + mouseY, mouseX, mouseY);
 }
 
-function drawGround() {
+function drawGround() {       // Draws the ground
   fill("green")
-  strokeWeight(5)
-  rect(0, height-100, width, 100 )
-  strokeWeight()
+  rect(0, height-100, width, 100 );
+  fill("black");
+  rect(0, height-100, width, 5);
+}
+function drawMount() {        // Draws the mountains
+  noStroke();
+  fill(190, 190, 190);
+  triangle(30, 350, 130, 100, 130, 350);
+  fill(170, 170, 170);
+  triangle(130, 350, 130, 100, 230, 350);
+  triangle(180, 350, 230, 235, 280, 350);
+  fill(190, 190, 190);
+  triangle(490, 350, 600, 150, 600, 350);
+  fill(170, 170, 170);
+  triangle(600, 350, 600, 150, 700, 350);
+  triangle(550, 350, 475, 200, 475, 350);
+  fill(190, 190, 190);
+  triangle(475, 350, 475, 200, 400, 350);      
+}
+function drawTree(x,y,s) {       // Draws the tree
+  let refX = x;
+  let refY = y;
+  let scale = s;
+  fill(0, 200, 50);
+  circle(refX, refY, 70*scale);
+  fill("brown");
+  rect(refX-10*scale, refY+20*scale, 20*scale, 60*scale);
+  circle(refX, refY+10*scale, 30*scale);
+
+
 }
