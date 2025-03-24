2f4d422281688f972eb1cf962fdc35cc41321df2
FHD2008 <fahadshah2008@outlook.com>
2025-03-24 12:17:26 -0600

    Update sketch.js

--- Cars Cars Cars assignment/sketch.js
+++ Cars Cars Cars assignment/sketch.js
@@ -30,10 +30,30 @@ class Vehicle{
   display(){
     this.x = width/2;
     this.y = 600;
+    let tireWidth = 15;
     //if(this.type === 0){
       fill(this.color);
-      rect(this.x, this.y, 70, 30);
-      rect(this.x+20, this.y+8, 40, 15);
+      //body
+      rect(this.x, this.y, 70, 30, 5, 0, 0, 5);
+      //roof
+      rect(this.x+18, this.y+6, 40, 18);
+      //windsheild
+      fill("skyblue");
+      rect(this.x + 7, this.y + 5, 10, 20, 5, 0, 0, 5);
+      fill(80, 80, 80);
+      //wheels
+      rect(this.x+5 , this.y - 2, tireWidth, 2);
+      rect(this.x + 50, this.y -2, tireWidth, 2);
+      rect(this.x + 5, this.y + 30, tireWidth, 2);
+      rect(this.x + 50, this.y + 30, tireWidth, 2);
+      //headlights
+      fill("white");
+      circle(this.x, this.y + 8, 3);
+      circle(this.x, this.y+22, 3);
+      //tailligths
+      fill("red");
+      rect(this.x + 68, this.y + 5, 2, 8);
+      rect(this.x+68, this.y + 18, 2, 8);
     //}
   }
 
