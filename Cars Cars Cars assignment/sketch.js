6bcd4cbe2071c04b370b7e227a896a4f03055c58
FHD2008 <fahadshah2008@outlook.com>
2025-03-25 12:17:20 -0600

    Update sketch.js

--- Cars Cars Cars assignment/sketch.js
+++ Cars Cars Cars assignment/sketch.js
@@ -20,41 +20,56 @@ class Vehicle{
   //1. Constructor
   constructor(dir){
     this.x = random(width);
-    this.y;
+    this.y = 
     this.direction = dir;
     this.xspeed;
-    this.type;
+    this.type = int(random(0,2));
     this.color = color(random(255), random(255), random(255));
   }
 
   display(){
-    this.x = width/2;
     this.y = 600;
     let tireWidth = 15;
-    //if(this.type === 0){
-      fill(this.color);
-      //body
-      rect(this.x, this.y, 70, 30, 5, 0, 0, 5);
-      //roof
-      rect(this.x+18, this.y+6, 40, 18);
-      //windsheild
-      fill("skyblue");
-      rect(this.x + 7, this.y + 5, 10, 20, 5, 0, 0, 5);
-      fill(80, 80, 80);
-      //wheels
-      rect(this.x+5 , this.y - 2, tireWidth, 2);
-      rect(this.x + 50, this.y -2, tireWidth, 2);
-      rect(this.x + 5, this.y + 30, tireWidth, 2);
-      rect(this.x + 50, this.y + 30, tireWidth, 2);
-      //headlights
-      fill("white");
-      circle(this.x, this.y + 8, 3);
-      circle(this.x, this.y+22, 3);
-      //tailligths
-      fill("red");
-      rect(this.x + 68, this.y + 5, 2, 8);
-      rect(this.x+68, this.y + 18, 2, 8);
-    //}
+    if(this.type === 0){
+       fill(this.color);
+       //body
+       rect(this.x, this.y, 70, 30, 5, 0, 0, 5);
+       //roof
+       rect(this.x+25, this.y+6, 30, 18, 0,4,4,0);
+       //windsheild
+       fill("skyblue");
+       rect(this.x + 17, this.y + 6, 8, 18, 4, 0, 0, 4);
+       fill(80, 80, 80);
+       //wheels
+       rect(this.x+5 , this.y - 2, tireWidth, 2);
+       rect(this.x + 50, this.y -2, tireWidth, 2);
+       rect(this.x + 5, this.y + 30, tireWidth, 2);
+       rect(this.x + 50, this.y + 30, tireWidth, 2);
+       //headlights
+       fill("white");
+       circle(this.x, this.y + 8, 3);
+       circle(this.x, this.y+22, 3);
+       //tailligths
+       fill("red");
+       rect(this.x + 68, this.y + 5, 2, 8);
+       rect(this.x+68, this.y + 18, 2, 8);
+    }
+    
+    else if(this.type === 1){
+       tireWidth = 10;
+       fill(this.color);
+       //body
+       rect(this.x, this.y, 70, 30, 3, 2, 2, 3);
+       //roof
+       fill("grey");
+       rect(this.x+11, this.y-2.5, 57, 35);
+       //wheels
+       
+       //headlights
+       fill("white");
+       circle(this.x, this.y + 8, 3);
+       circle(this.x, this.y+22, 3);
+    }
   }
 
   move(){
