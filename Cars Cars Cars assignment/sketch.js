b3f6c855068ab6016165e7d51121b1f96c264dbf
FHD2008 <fahadshah2008@outlook.com>
2025-03-28 12:48:51 -0600

    Update sketch.js

--- Cars Cars Cars assignment/sketch.js
+++ Cars Cars Cars assignment/sketch.js
@@ -4,23 +4,25 @@
 // 
 
 let car;
+let trafficLight;
 let eastBound=[];
 let westBound= [];
 
-function setup() {
+function setup() { 
   createCanvas(windowWidth, windowHeight);
+  trafficLight = new TrafficLight(width-100, 100);
   
   for (let i = 0; i<20; i++){
     eastBound.push(new Vehicle(0, random(height/4, height/2 -40)));
-  }
-  for (let i = 0; i<20; i++){
     westBound.push(new Vehicle(1, random(height/2, 3*height/4 - 40)));
   }
-}
+} 
 
 function draw() {
+  nFrame = 0;
   background(220);
   drawRoad();
+  trafficLight.display();
   for (let i of eastBound){
     i.action();
   }
@@ -29,6 +31,20 @@ function draw() {
   }
 
 }
+
+function mousePressed(){
+  if (keyIsDown(SHIFT)){
+    westBound.push(new Vehicle(1, random(height/2, 3*height/4 - 40)));
+  }
+  else{
+    eastBound.push(new Vehicle(0, random(height/4, height/2 -40)));
+  }
+  
+}
+
+
+
+
 
 class Vehicle{
   //1. Constructor
@@ -40,10 +56,10 @@ class Vehicle{
     this.type = int(random(0,2));
     this.color = color(random(255), random(255), random(255));
   }
-
-  display(){
+  //display function renders everything in the canvas
+  display(){     
     let tireWidth = 10;
-    if(this.type === 0){
+    if(this.type === 0){       //car
        fill(this.color);
        push();
        translate(this.x, this.y);
@@ -54,20 +70,20 @@ class Vehicle{
        //body
        rect(0, 0, 70, 30, 5, 0, 0, 5);
        //roof
-       rect(0+25, 0+2.5, 30, 25, 0,4,4,0);
+       rect(25, 2.5, 30, 25, 0,4,4,0);
        //windsheild
        fill("skyblue");
-       rect(0+17, 0 + 3.5, 8, 23, 4, 0, 0, 4);
+       rect(17, 3.5, 8, 23, 4, 0, 0, 4);
        fill(80, 80, 80);
        //wheels
-       rect(0+7 , 0 - 2, tireWidth, 2);
-       rect(0 + 55, 0 -2, tireWidth, 2);
-       rect(0 + 7, 0 + 30, tireWidth, 2);
-       rect(0 + 55, 0 + 30, tireWidth, 2);
+       rect(7 , -2, tireWidth, 2);
+       rect( 55, -2, tireWidth, 2);
+       rect( 7,30, tireWidth, 2);
+       rect( 55, 30, tireWidth, 2);
        //headlights
        fill("white");
-       circle(0, 0 + 8, 3);
-       circle(0, 0+22, 3);
+       circle(2,  8, 3);
+       circle(2, 22, 3);
        //tailligths
        fill("red");
        rect(0 + 67, 0 + 2, 2, 6, 5,0,0,0);
@@ -75,7 +91,7 @@ class Vehicle{
        pop();
     }
     
-    else if(this.type === 1){
+    else if(this.type === 1){     //truck
        tireWidth = 10;
        fill(this.color);
        push();
@@ -97,7 +113,7 @@ class Vehicle{
        pop();
     }
   }
-
+  //movement of cars and trucks
   move(){
     if (this.direction === 0){
       this.x += this.xSpeed;
@@ -107,32 +123,34 @@ class Vehicle{
     }
 
   }
-
+  // speeding up of vehicles
   speedUp(){
     if (this.xSpeed < 15){
       this.xSpeed += 1;
     }
   }
-
+  //slowing down of vehicles
   speedDown(){
     if (this.xSpeed > 1){
       this.xSpeed -= 1;
     }
   }
-
+  //the car changes color randomly 
   changeColor(){
     this.color = color(random(255), random(255), random(255));
   }
 
   action(){
+    //
     this.display();
-    
+    //wrap around
     if (this.x < -60){
       this.x = width;
     }
     if (this.x > width+60){
       this.x = -60;
     }
+    // calling the rest of the function with 1% chance
     if (random(100) < 1){
       this.speedUp();
     }
@@ -142,18 +160,48 @@ class Vehicle{
     if (random(100) < 1){
       this.changeColor();
     }
+    //
     this.move();
-
-
   }
 
 
 }
 
-function drawRoad(){
+class TrafficLight{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.signalColor = "green";
+  }
+
+  display(){
+    fill("black");
+    rect(this.x, this.y, 28, 70, 4);
+    fill(this.signalColor);
+    if (this.signalColor === "green"){
+      circle(this.x + 14, this.y+55, 20);
+    }
+    if (this.signalColor === "red"){
+      circle(this.x + 14, this.y+15, 20);
+    }
+  }
+
+  turnRed(){
+    this.signalColor = "red";
+  }
+  turnGreen(){
+    this.signalColor = "green";
+  }
+
+  action(){
+    this.display()
+  }
+}
+
+function drawRoad(){   //renders road
   fill(70);
   rect(0, height/4, width, height/2);
-  for (let x = 0; x <= width; x += 45){
+  for (let x = 0; x <= width; x += 45){  //loop to make separation line
     fill("Yellow");
     rect(x, height/2, 25, 5);
   }
