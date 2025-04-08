74f2ee4d5a541a9c38c6e119daefd1fc53d8daf7
FHD2008 <fahadshah2008@outlook.com>
2025-04-08 12:12:14 -0600

    added overlay

--- Puzzle Game/sketch.js
+++ Puzzle Game/sketch.js
@@ -16,7 +16,6 @@ let gridData = [[0,0,0,0,0],
 
 
 function setup() {
-  // Determine the size of each square. Could use windowHeight,windowHeight  for Canvas to keep a square aspect ratio
   createCanvas(windowWidth, windowHeight);
   randomGrid();
   rectWidth = width/NUM_COLS;
@@ -25,8 +24,8 @@ function setup() {
 
 function draw() {
   background(220);
-  determineActiveSquare();   //figure out which tile the mouse cursor is over
-  drawGrid();                //render the current game board to the screen (and the overlay)
+  determineActiveSquare();   //determine which tile the mouse cursor is on
+  drawGrid();                //draws the grid board on screen
   if (checkForWin() === true){
     fill("Green");
     textSize(75);
@@ -34,16 +33,18 @@ function draw() {
     textFont("Times New Roman");
     text("VICTORY!!", width/2, height/2);
   }
+  cursorOverlay()
+ 
 }
 
 
 
-function mousePressed(){
-  // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked within the flip function to ensure in-bounds access for array
-  if(keyIsDown(SHIFT)){
+function mousePressed(){ //flips the tiles in cross shape pattern 
+  if(keyIsDown(SHIFT)){         //for cheater function, only flips the tile mouse is on
     flip(currentCol, currentRow);
   }
-  else{
+  else{                         //flips the current tile as well as the four in each direction around it(cross pattern)
+    flip(currentCol, currentRow)
     flip(currentCol-1, currentRow);
     flip(currentCol+1, currentRow);
     flip(currentCol, currentRow-1);
@@ -52,9 +53,8 @@ function mousePressed(){
   
 }
 
-function flip(col, row){
-  // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
-  // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
+function flip(col, row){ //flip the tiles in columns and rows flip from 0 to 255 or 255 to 0.
+  
   if (col >= 0 && col < NUM_COLS ){
     if (row >= 0 && row < NUM_ROWS){
       if (gridData[row][col] === 0) gridData[row][col] = 255;
@@ -63,14 +63,15 @@ function flip(col, row){
   }
 }
 
-function determineActiveSquare(){
-  // An expression to run each frame to determine where the mouse currently is.
+function determineActiveSquare(){    // check location of mouse and which sqaure it's on.
   currentRow = int(mouseY / rectHeight);
   currentCol = int(mouseX / rectWidth);
+  
+  
 }
 
 function drawGrid(){
-  // Render a grid of squares - fill color set according to data stored in the 2D array
+  //draws grid of squares from the data in 2d array
   for (let x = 0; x < NUM_COLS ; x++){
     for (let y = 0; y < NUM_ROWS; y++){
       fill(gridData[y][x]); 
@@ -79,7 +80,7 @@ function drawGrid(){
   }
 }
 
-function checkForWin(){
+function checkForWin(){    //checks if the user won the game by getting all the squares same color (white)
   for (let y = 0; y < NUM_ROWS ; y++){
     for (let x = 0; x < NUM_COLS; x++){
       if(gridData[y][x] !== 255){
@@ -96,5 +97,22 @@ function randomGrid(){
       gridData[y][x] = random([0, 255]);
     }
   }
+}
+
+function cursorOverlay(){     //creates green overlay on the squares being affected by mouse click
+  fill(144,238,144, 150);
+  if (checkForWin() !== true){  //ensures the overlay seen only when game is in action not when won
+    if (keyIsDown(SHIFT)){  //only have overlay on current square cursor is on
+      rect(currentCol*rectWidth, currentRow*rectHeight, rectWidth, rectHeight);
+    }
+    else{ //overlay on every square going to be affected by mouse click
+      rect(currentCol*rectWidth, currentRow*rectHeight, rectWidth, rectHeight);
+      rect((currentCol+1)*rectWidth, currentRow*rectHeight, rectWidth, rectHeight);
+      rect((currentCol-1)*rectWidth, currentRow*rectHeight, rectWidth, rectHeight);
+      rect(currentCol*rectWidth, (currentRow+1)*rectHeight, rectWidth, rectHeight);
+      rect(currentCol*rectWidth, (currentRow-1)*rectHeight, rectWidth, rectHeight);
+    }
+  }
+  
 }
 
