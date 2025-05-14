8d1f95987f3626b351e1726e848b1d8dd8f9e5b1
FHD2008 <fahadshah2008@outlook.com>
2025-05-13 23:04:49 -0600

    Player Animation, tile textures
    Added other player animations like falling, moving, jumping also implemented tile texture to create the map seamlessly and added feature of jumping down through the one way platforms.

---
+++ capstone-coding-project/Assets/Code/player_animator.gd
@@ -0,0 +1,20 @@
+extends Node2D
+@export var player_Controller: PlayerController
+@export var sprite: AnimatedSprite2D
+
+func _process(_delta):
+	if player_Controller.direction == 1:
+		sprite.flip_h = false
+	elif player_Controller.direction == -1:
+		sprite.flip_h = true
+		
+	if abs(player_Controller.velocity.x) > 0.0:
+		sprite.play("moving_animation")
+	else:
+		sprite.play("idle animation") 
+		
+	if player_Controller.is_on_floor() == false && player_Controller.velocity.y < 0.0:
+		sprite.play("jump_animation")
+	elif player_Controller.is_on_floor() == false && player_Controller.velocity.y > 0.0:
+		sprite.play("fall_animation")
+	
--- capstone-coding-project/Assets/Code/player_controller.gd
+++ capstone-coding-project/Assets/Code/player_controller.gd
@@ -1,8 +1,21 @@
 extends CharacterBody2D
+class_name PlayerController
 
+@export var speed = 10
+@export var jump_power = 10
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var direction = 0
+var speed_Multiplier = 30.0
+var jump_Multiplier = -35.0
+
+func _input(event):
+	# Handle jump.
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_Multiplier
+	if event.is_action_pressed("jump_downwards") and is_on_floor():
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
 
 
 func _physics_process(delta: float) -> void:
@@ -10,16 +23,14 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed * speed_Multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * speed_Multiplier)
 
 	move_and_slide()
