da4902cf2c65047bf7f1d689d3de4ac3b127b72a
FHD2008 <fahadshah2008@outlook.com>
2025-05-14 21:54:45 -0600

    Moving Platforms(continued), and level template
    Finished the moving platforms part(tutorial part) created a level template and inherited new scenes from it to make new levels based on it.

--- capstone-coding-project/Assets/Code/moving_platform.gd
+++ capstone-coding-project/Assets/Code/moving_platform.gd
@@ -2,13 +2,16 @@ extends Path2D
 class_name moving_platform
 
 @export var path_follow_2d: PathFollow2D
+@export var move_speed =  1.0
+@export var ease: Tween.EaseType
+@export var transition: Tween.TransitionType
 
 func _ready():
 	move_tween()
 	
 func move_tween():
 	var tween = get_tree().create_tween().set_loops()
-	tween.tween_property(path_follow_2d, "progress_ratio", 1.0, 1.5)
-	tween.tween_property(path_follow_2d, "progress_ratio", 0.0, 1.5)
+	tween.tween_property(path_follow_2d, "progress_ratio", 1.0, move_speed).set_ease(ease).set_trans(transition)
+	tween.tween_property(path_follow_2d, "progress_ratio", 0.0, move_speed).set_ease(ease).set_trans(transition)
 	
 	
