badf62402bf337ed1581abf1a9246d5674600260
FHD2008 <fahadshah2008@outlook.com>
2025-05-19 23:11:06 -0600

    added new background scene

--- capstone-coding-project/Assets/Code/player_controller.gd
+++ capstone-coding-project/Assets/Code/player_controller.gd
@@ -7,7 +7,7 @@ class_name PlayerController
 var direction = 0
 var speed_Multiplier = 30.0
 var jump_Multiplier = -35.0
-var time = 0
+var wall_gravity = 0.5
 
 func _input(event):
 	# Jumping up functionality
@@ -24,14 +24,11 @@ func _input(event):
 		set_collision_mask_value(10, true)
 		
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
-	time = delta
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-
-	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -41,5 +38,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_Multiplier)
 
+	
 
 	move_and_slide()
