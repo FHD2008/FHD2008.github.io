a770908f8236bdc767c8d16dd7d5f3c3d1bfbc55
FHD2008 <fahadshah2008@outlook.com>
2025-06-03 12:17:49 -0600

    Added enemy and levels page
    On menu screen I added a level page button and removed settings button. And I also created an enemy scene.

--- capstone_coding_project/Assets/Scenes/UI Scenes/starting_screen.gd
+++ capstone_coding_project/Assets/Scenes/UI Scenes/starting_screen.gd
@@ -1,8 +1,21 @@
 extends Control
 @export var play_button: Button
+@export var levels_button: Button
+@export var return_button: Button
 
 func _ready():
 	play_button.pressed.connect(start_game)
+	levels_button.pressed.connect(open_levels_page)
+	return_button.pressed.connect(return_to_home)
+	return_button.visible = false
 	
 func start_game():
 	get_tree().change_scene_to_file("res://Assets/Scenes/gameplay.tscn")
+	
+func open_levels_page():
+	return_button.visible = true
+	get_tree().change_scene_to_file("res://Assets/Scenes/UI Scenes/levels_page.tscn")
+	
+
+func return_to_home():
+	get_tree().change_scene_to_file("res://Assets/Scenes/UI Scenes/Starting_screen.tscn")
